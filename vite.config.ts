0f958d9466983a239aaab136fa22b468d19bd05a
rss042code@gmail.com <rss042code@gmail.com>
2026-07-18 14:32:18 +0300

    new file mode 100644

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/nttaxi/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
